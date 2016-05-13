319b0d7565e946e610c71ec6be69d9bd078f15d2
Vitaly Markov <v.y.markov@gmail.com>
2016-05-13 23:06:51 +0300

    :rocket: Update the security group

--- terraform/consul/aws_sg.tf
+++ terraform/consul/aws_sg.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "docker" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "consul" {
